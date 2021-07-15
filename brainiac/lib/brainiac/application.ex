39b828c2a15f671ced8a4a68bb82e36c85ac089d
Aniket Chauhan <aniket.chauhan@priceline.com>
2021-07-15 12:00:27 -0400

    add supervisor implementation

--- brainiac/lib/brainiac/application.ex
+++ brainiac/lib/brainiac/application.ex
@@ -9,13 +9,16 @@ defmodule Brainiac.Application do
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: Brainiac.Worker.start_link(arg)
-      {Brainiac.Server, :prabhakar}
+      {Brainiac.Server, Brainiac.Server},
+      {Brainiac.Server, :batman},
+      {Brainiac.Server, :superman},
+      {Brainiac.Server, :green_lantern}
       # {DynamicSupervisor, strategy: :one_for_one, name: Brainiac.DynamicSupervisor}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Brainiac.Supervisor]
+    opts = [strategy: :rest_for_one, name: Brainiac.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
